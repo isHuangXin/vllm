8314c0121100c3dc11c2158474b5615f908377a1
isHuangXin <597934012@qq.com>
2026-03-05 18:41:44 +0800

    [updaet] add vllm-mooncake connector _init_

---
+++ vllm/distributed/kv_transfer/kv_connector/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from vllm.distributed.kv_transfer.kv_connector.v1 import mooncake  # noqa: F401
+
+__all__ = ["mooncake"]
